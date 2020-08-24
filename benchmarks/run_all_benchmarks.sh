b06a7677e7ae6de1e4519383a682b607e25b2c75
Jiusheng Chen <chenjiusheng@outlook.com>
2020-08-24 06:05:41 +0000

    Refine benchmarks.

--- benchmarks/run_all_benchmarks.sh
+++ benchmarks/run_all_benchmarks.sh
@@ -47,40 +47,41 @@ grep -E "transformers_v3.0.2\+fastseq_v.* facebook/bart-large-cnn cnn_dm/raw val
 grep -E "transformers_v3.0.2\+fastseq_v.* facebook/bart-large-cnn cnn_dm/raw val 64 " perf | awk '{s+=$13}END{print s/NR}' | bash range.sh 4.5 100
 
 # MODEL - distibart cnn
-./benchmark.sh transformers sshleifer/distilbart-cnn-12-6 cnn_dm/raw val 64/128/256 --task summarization  # take 2.5 hours
-./benchmark.sh transformers+fastseq sshleifer/distilbart-cnn-12-6 cnn_dm/raw val 64/128/256/512 --task summarization  # take 2.5 hours
+./benchmark.sh transformers sshleifer/distilbart-cnn-12-6 cnn_dm/raw val 64/128 --task summarization  # take 2.5 hours
+./benchmark.sh transformers+fastseq sshleifer/distilbart-cnn-12-6 cnn_dm/raw val 64/128/256 --task summarization  # take 2.5 hours
 # Accuracy
-grep "sshleifer/distilbart-cnn-12-6 cnn_dm/raw val " perf | awk '{if($9!="NA"){c+=1;s+=$9}}END{print s/c}' | bash range.sh 45 45.2
+grep "sshleifer/distilbart-cnn-12-6 cnn_dm/raw val " perf | awk '{if($9!="NA"){c+=1;s+=$9}}END{print s/c}' | bash range.sh 45 45.1
 # Speed on V100 16GB 250W
-grep -E "transformers_v3.0.2 sshleifer/distilbart-cnn-12-6 cnn_dm/raw val 64 " perf | awk '{s+=$13}END{print s/NR}' | bash range.sh 3.0 3.2
-grep -E "transformers_v3.0.2\+fastseq_v.* sshleifer/distilbart-cnn-12-6 cnn_dm/raw val 64 " perf | awk '{s+=$13}END{print s/NR}' | bash range.sh 6.1 100
+grep -E "transformers_v3.0.2 sshleifer/distilbart-cnn-12-6 cnn_dm/raw val 64 " perf | awk '{s+=$13}END{print s/NR}' | bash range.sh 2.95 3.05
+grep -E "transformers_v3.0.2\+fastseq_v.* sshleifer/distilbart-cnn-12-6 cnn_dm/raw val 64 " perf | awk '{s+=$13}END{print s/NR}' | bash range.sh 5.2 100
+# todo: bigger bs doesn't increase speed
+grep -E "transformers_v3.0.2\+fastseq_v.* sshleifer/distilbart-cnn-12-6 cnn_dm/raw val 128 " perf | awk '{s+=$13}END{print s/NR}' | bash range.sh 5.2 100
 
 # MODEL - wmt16
 ./benchmark.sh fairseq wmt16.en.de.32k wmt16_en_de_bpe32k/bin test 256/512       # take 20 minutes
-# broken now
-#./benchmark.sh fairseq+fastseq wmt16.en.de.32k wmt16_en_de_bpe32k/bin test 256/512/1024/2048  # take 20 minites
+./benchmark.sh fairseq+fastseq wmt16.en.de.32k wmt16_en_de_bpe32k/bin test 256/512/1024/2048  # take 20 minites
 # Accuracy
 grep " wmt16.en.de.32k wmt16_en_de_bpe32k/bin test " perf | awk '{if($8!="NA"){c+=1;s+=$8}}END{print s/c}' | bash range.sh 0.179 0.181 
 # Speed on V100 16GB 250W
 grep -E "fairseq_v0.9.0 wmt16.en.de.32k wmt16_en_de_bpe32k/bin test 256 " perf | awk '{s+=$13}END{print s/NR}' | bash range.sh 49 51
-#grep -E "fairseq_v0.9.0\+fastseq_v.* wmt16.en.de.32k wmt16_en_de_bpe32k/bin test 1024 " perf | awk '{s+=$13}END{print s/NR}' | bash range.sh 69 100
+grep -E "fairseq_v0.9.0\+fastseq_v.* wmt16.en.de.32k wmt16_en_de_bpe32k/bin test 1024 " perf | awk '{s+=$13}END{print s/NR}' | bash range.sh 75 100
 
-## MODEL - t5
-./benchmark.sh transformers t5-base wmt_en_ro/raw val 64/128/256/512/1024 --task translation_en_to_ro          # each bs takes 5 minutes
-# broken now
-#./benchmark.sh transformers+fastseq t5-base wmt_en_ro/raw val 64/128/256/512/1024 --task translation_en_to_ro  # each bs takes 5 minutes
+# MODEL - t5
+./benchmark.sh transformers t5-base wmt_en_ro/raw val 64/128 --task translation_en_to_ro          # each bs takes 5 minutes
+./benchmark.sh transformers+fastseq t5-base wmt_en_ro/raw val 64/128 --task translation_en_to_ro  # each bs takes 5 minutes
 # Accuracy
-grep "t5-base wmt_en_ro/raw val " perf | awk '{if($9!="NA"){c+=1;s+=$9}}END{print s/c}' | bash range.sh 26.9 26.92
+grep "t5-base wmt_en_ro/raw val " perf | awk '{if($8!="NA"){c+=1;s+=$8}}END{print s/c}' | bash range.sh 27.82 27.84
 # Speed on V100 16GB 250W
-grep -E "transformers_v3.0.2 t5-base wmt_en_ro/raw val 64 " perf | awk '{s+=$13}END{print s/NR}' | bash range.sh 3.5 4
-#grep -E "transformers_v3.0.2\+fastseq_v.* t5-base wmt_en_ro/raw val 64 " perf | awk '{s+=$13}END{print s/NR}' | bash range.sh 100 100
+grep -E "transformers_v3.0.2 t5-base wmt_en_ro/raw val 64 " perf | awk '{s+=$13}END{print s/NR}' | bash range.sh 7 7.5
+# todo: 1, fastseq max bs is still 64. 2, no speed gain.
+grep -E "transformers_v3.0.2\+fastseq_v.* t5-base wmt_en_ro/raw val 64 " perf | awk '{s+=$13}END{print s/NR}' | bash range.sh 7 7.5
 
 # MODEL - mbart
 ./benchmark.sh transformers facebook/mbart-large-en-ro wmt_en_ro/raw val 64/128 --task translation    # each bs takes 5 minutes
 ./benchmark.sh transformers+fastseq facebook/mbart-large-en-ro wmt_en_ro/raw val 64/128/256/512 --task translation    # each bs takes 5 minutes
 # Accuracy
-# broken now, fastseq result is different, bleu is higher 27.80 vs 27.89
-#grep "facebook/mbart-large-en-ro wmt_en_ro/raw val " perf | awk '{if($8!="NA"){c+=1;s+=$8}}END{print s/c}' | bash range.sh 27.79 27.81
+# todo, fastseq result is different, bleu is higher 27.80 vs 27.89
+grep "facebook/mbart-large-en-ro wmt_en_ro/raw val " perf | awk '{if($8!="NA"){c+=1;s+=$8}}END{print s/c}' | bash range.sh 27.79 27.81
 # Speed on V100 16GB 250W
 grep -E "transformers_v3.0.2 facebook/mbart-large-en-ro wmt_en_ro/raw val 32 " perf | awk '{s+=$13}END{print s/NR}' | bash range.sh 5.8 6.2
 grep -E "transformers_v3.0.2\+fastseq_v.* facebook/mbart-large-en-ro wmt_en_ro/raw val 32 " perf | awk '{s+=$13}END{print s/NR}' | bash range.sh 6.0 100
