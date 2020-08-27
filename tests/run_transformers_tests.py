7bd6a0d8f3156865de368b72c93dfde0dd92eda9
NickNickGo <nibhenda@microsoft.com>
2020-08-27 00:58:04 +0000

    adding parametrized unit tests

--- tests/run_transformers_tests.py
+++ tests/run_transformers_tests.py
@@ -1,4 +1,4 @@
-""" script for importing fairseq tests """
+""" script for importing transformers tests """
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
@@ -13,29 +13,29 @@ from git import Repo
 from absl.testing import absltest, parameterized
 
 FASTSEQ_PATH = '/'.join(os.path.realpath(__file__).split('/')[0:-2])
-FAIRSEQ_PATH = '/tmp/fairseq/'
-FAIRSEQ_GIT_URL = 'https://github.com/pytorch/fairseq.git'
+TRANSFORMERS_PATH = '/tmp/transformers/'
+TRANSFORMERS_GIT_URL = 'https://github.com/huggingface/transformers.git'
 
 
 class FairseqUnitTests(parameterized.TestCase):
     def prepare_env(self):
         """set env variables"""
         original_pythonpath = os.environ['PYTHONPATH'] if 'PYTHONPATH' in os.environ else '' 
-        os.environ['PYTHONPATH'] = FAIRSEQ_PATH + ':' + original_pythonpath
+        os.environ['PYTHONPATH'] = TRANSFORMERS_PATH + ':' + original_pythonpath
         #Removing following path since it contains utils directory
-        #which clashes with utils.py file in fairseq/tests.
+        #which clashes with utils.py file in transformers/tests.
         if FASTSEQ_PATH in sys.path:
             sys.path.remove(FASTSEQ_PATH)
         if '' in sys.path:
             sys.path.remove('')
-        sys.path.insert(0, FAIRSEQ_PATH)
+        sys.path.insert(0, TRANSFORMERS_PATH)
     
     
-    def clone_fairseq(self, repo, version):
-        """clone fairseq repo"""
-        if os.path.isdir(FAIRSEQ_PATH):
-            shutil.rmtree(FAIRSEQ_PATH)
-        Repo.clone_from(FAIRSEQ_GIT_URL, FAIRSEQ_PATH, branch=version)
+    def clone_transformers(self, repo, version):
+        """clone transformers repo"""
+        if os.path.isdir(TRANSFORMERS_PATH):
+            shutil.rmtree(TRANSFORMERS_PATH)
+        Repo.clone_from(TRANSFORMERS_GIT_URL, TRANSFORMERS_PATH, branch=version)
     
     
     def get_test_suites(self, test_files_path, blocked_tests):
@@ -55,21 +55,20 @@ class FairseqUnitTests(parameterized.TestCase):
     
     @parameterized.named_parameters({
             'testcase_name': 'Normal',
-            'without_fastseq_opt': True,
-            'fairseq_version': 'v0.9.0',
-            'blocked_tests':['test_binaries.py', 'test_bmuf.py',
-                            'test_reproducibility.py'
+            'without_fastseq_opt': False,
+            'transformers_version':'v3.0.2',
+            'blocked_tests':[
                             ]
         })
-    def test_suites(self, without_fastseq_opt, fairseq_version, blocked_tests):
+    def test_suites(self, without_fastseq_opt, transformers_version, blocked_tests):
     
         if not without_fastseq_opt:
             import fastseq  #pylint: disable=import-outside-toplevel
     
         self.prepare_env()
-        self.clone_fairseq(FAIRSEQ_GIT_URL, fairseq_version)
+        self.clone_transformers(TRANSFORMERS_GIT_URL, transformers_version)
     
-        test_files_path = FAIRSEQ_PATH + '/tests/test_*.py'
+        test_files_path = TRANSFORMERS_PATH + '/tests/test_*.py'
         suites = self.get_test_suites(test_files_path, blocked_tests)
         test_suite = unittest.TestSuite(suites)
         test_runner = unittest.TextTestRunner().run(test_suite)
