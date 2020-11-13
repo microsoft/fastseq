83154f10acbacecd8bcd2524f066211252369aed
Fei Hu <hufei68@gmail.com>
2020-11-13 05:46:26 +0000

    Make einsum_rewrite_pattern more general

--- fastseq/optimizer/jit/einsum_rewriter.py
+++ fastseq/optimizer/jit/einsum_rewriter.py
@@ -16,8 +16,11 @@ def einsum_pattern_0(t0: str, t1: List[Tensor]):
     return r
 
 @graph_pattern
-def einsum_rewrite_pattern_0(equation: str, operands: List[Tensor]):
-    if equation == "bmhtd,bnhsd->bmhts":
+def einsum_rewrite_pattern_0(eqn: str, operands: List[Tensor]):
+    # for cases like "bmhtd,bnhsd->bmhts"
+    if (len(eqn) == 18 and eqn[0:3] == eqn[13:16] and eqn[0] == eqn[6] and
+        eqn[2] == eqn[8] and eqn[4] == eqn[10] and eqn[3] == eqn[16] and
+        eqn[9] == eqn[17]):
         t0 = operands[0]
         t1 = operands[1]
         b = t0.size(0)
@@ -26,15 +29,20 @@ def einsum_rewrite_pattern_0(equation: str, operands: List[Tensor]):
         t = t0.size(3)
         d = t0.size(4)
         n = t1.size(1)
+        if n > 1:
+            t1 = t1.sum(dim=1, keepdim=True) # (b, 1, h, d, s)
         s = t1.size(3)
         t0 = t0.permute(0, 2, 1, 3, 4) # (b, h, m, t, d)
-        t1 = t1.permute(0, 2, 4, 1, 3) # (b, h, d, n, s)
+        t1 = t1.permute(0, 2, 1, 4, 3) # (b, h, 1, d, s)
         t0 = t0.reshape(b*h, m*t, d)
-        t1 = t1.reshape(b*h, d, n*s) # TODO: add a check: assert n == 1
-        r = torch.bmm(t0, t1).view(b, h, m, t, n*s).permute(0, 2, 1, 3, 4)
+        t1 = t1.reshape(b*h, d, s)
+        r = torch.bmm(t0, t1).view(b, h, m, t, s).permute(0, 2, 1, 3, 4)
         return r
 
-    if equation == "bmhts,bnhsd->bmhtd":
+    # for cases like "bmhts,bnhsd->bmhtd"
+    if (len(eqn) == 18 and eqn[0:3] == eqn[13:16] and eqn[0] == eqn[6] and
+        eqn[2] == eqn[8] and eqn[4] == eqn[9] and eqn[3] == eqn[16] and
+        eqn[10] == eqn[17]):
         t0 = operands[0]
         t1 = operands[1]
         b = t0.size(0)
@@ -43,15 +51,17 @@ def einsum_rewrite_pattern_0(equation: str, operands: List[Tensor]):
         t = t0.size(3)
         s = t0.size(4)
         n = t1.size(1)
+        if n > 1:
+            t1 = t1.sum(dim=1, keepdim=True) # (b, 1, h, s, d)
         d = t1.size(4)
         t0 = t0.permute(0, 2, 1, 3, 4) # (b, h, m, t, s)
-        t1 = t1.permute(0, 2, 3, 1, 4) # (b, h, s, n, d)
+        t1 = t1.permute(0, 2, 1, 3, 4) # (b, h, 1, s, d)
         t0 = t0.reshape(b*h, m*t, s)
-        t1 = t1.reshape(b*h, s, n*d) # TODO: add a check: assert n == 1
-        r = torch.bmm(t0, t1).view(b, h, m, t, n*d).permute(0, 2, 1, 3, 4)
+        t1 = t1.reshape(b*h, s, d)
+        r = torch.bmm(t0, t1).view(b, h, m, t, d).permute(0, 2, 1, 3, 4)
         return r
 
-    return torch.einsum(equation, operands)
+    return torch.einsum(eqn, operands)
 
 EINSUM_PATTERN_STR = einsum_pattern_0()
 EINSUM_REWRITE_PATTERN_STR = einsum_rewrite_pattern_0()
