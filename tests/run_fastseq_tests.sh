b67e0b7d721d6796cb4f4e3f4d96a2998a86d14f
Fei Hu <hufei68@gmail.com>
2020-11-16 20:53:22 +0000

    Just use a small unit test to test pipeline

--- tests/run_fastseq_tests.sh
+++ tests/run_fastseq_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for fastseq_py_test_file in $(find tests/ -name "test_*.py")
+for fastseq_py_test_file in $(find tests/ -name "test_api_*.py")
 do
   echo "run $fastseq_py_test_file"
   python $fastseq_py_test_file
