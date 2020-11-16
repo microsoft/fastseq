c6da60695cc4d6e4018412aa9f5da17a9498d94e
Fei Hu <hufei68@gmail.com>
2020-11-16 20:08:25 +0000

    Add Nikhil's changes on pipeline to publish XML

--- tests/run_fastseq_tests.sh
+++ tests/run_fastseq_tests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-for fastseq_py_test_file in $(find tests/ -name "*test_*.py")
+for fastseq_py_test_file in $(find tests/ -name "test_*.py")
 do
-  echo $fastseq_py_test_file
+  echo "run $fastseq_py_test_file"
+  python $fastseq_py_test_file
 done
