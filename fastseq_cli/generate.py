d4e08c5a2637c3313398c520496b2fe18db2dbc2
Fei Hu <hufei68@gmail.com>
2021-06-30 19:23:52 +0000

    Fix the parser issue in supporting prophetnet task

--- fastseq_cli/generate.py
+++ fastseq_cli/generate.py
@@ -6,34 +6,25 @@ Translate pre-processed data with a trained model and the optimizations provided
 by FastSeq.
 """
 
-from fairseq import options
 import os
-import argparse
+import sys
 
-def parse_additional_args ():
+from fairseq import options
+
+def parse_additional_args():
     parser = options.get_generation_parser()
-    parser.add_argument('--use-el-attn', action='store_true',
-            help='Use Efficient Lossless Attention optimization ? ')
     parser.add_argument(
-        '--postprocess-workers',
-        default=1,
-        type=int,
-        choices=range(1, 128, 1),
-        metavar='N',
-        help='number of worker for post process')
-    parser.add_argument(
-        '--decode-hypothesis',
-        action="store_true")
+        '--use-el-attn',
+        action='store_true',
+        help='Use Efficient Lossless Attention optimization ? ')
     args = options.parse_args_and_arch(parser)
     return args
 
 def cli_main():
+    os.environ['USE_EL_ATTN'] = '1' if '--use-el-attn' in sys.argv else '0'
+    from fastseq.optimizer.fairseq.generate import main_v1  # pylint: disable=import-outside-toplevel
     args = parse_additional_args()
-    os.environ['USE_EL_ATTN'] = '1' if args.use_el_attn else '0'
-    import fastseq
-    from fastseq.optimizer.fairseq.generate import main_v1
     main_v1(args)
- 
+
 if __name__ == '__main__':
     cli_main()
-
