556d868b243c9ce83104373b06bed5a9c91a77b7
Julianne Knott <julesk1998@gmail.com>
2021-10-25 23:00:13 +0000

    debug pipeline

--- tests/run_fastseq_tests.sh
+++ tests/run_fastseq_tests.sh
@@ -2,10 +2,11 @@
 
 for fastseq_py_test_file in $(find tests/ -name "test_*.py")
 do 
-  if [[ $fastseq_py_test_file == *"test_generate_fs_cli"* ]]; then
-        echo "Running $fastseq_py_test_file"
-        python $fastseq_py_test_file
-  fi
+  echo "Skipping $fastseq_py_test_file"
+  # if [[ $fastseq_py_test_file == *"test_generate_fs_cli"* ]]; then
+  #       echo "Running $fastseq_py_test_file"
+  #       python $fastseq_py_test_file
+  # fi
   # if [[ $fastseq_py_test_file == *"test_fairseq_optimizer"* ]]; then
   #     echo "Running $fastseq_py_test_file with EL Attention"
   #     USE_EL_ATTN=1 python $fastseq_py_test_file
