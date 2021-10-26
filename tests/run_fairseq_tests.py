60cb8e422a3f808cf9b99c0872bf72e35fad5de4
Julianne Knott <julesk1998@gmail.com>
2021-10-26 17:20:39 +0000

    debug pipeline

--- tests/run_fairseq_tests.py
+++ tests/run_fairseq_tests.py
@@ -31,7 +31,8 @@ class FairseqUnitTests(parameterized.TestCase):
         if FASTSEQ_PATH in sys.path:
             sys.path.remove(FASTSEQ_PATH)
         sys.path.insert(0, FAIRSEQ_PATH)
-        sys.path.insert(0 , '/tmp/')
+        sys.path.insert(0, '/tmp/')
+        print("PATH after prepare env: " + os.environ['PATH'])
 
     def clone_and_build_fairseq(self, repo, version):
         """clone and build fairseq repo"""
@@ -40,6 +41,13 @@ class FairseqUnitTests(parameterized.TestCase):
         Repo.clone_from(FAIRSEQ_GIT_URL, FAIRSEQ_PATH, branch=version)
         pipmain(['install', 'git+https://github.com/pytorch/fairseq.git@' +
                   version])
+        print("FAIRSEQ_PATH: " + FAIRSEQ_PATH)
+        directory_path = os.getcwd()
+        print("My current directory is : " + directory_path)
+        folder_name = os.path.basename(directory_path)
+        print("My directory name is : " + folder_name)
+        print("Contents: " + "\t".join([item for item in os.scandir(os.getcwd())]))
+        print("PATH: " + os.environ['PATH'])
         original_pythonpath = os.environ[
             'PYTHONPATH'] if 'PYTHONPATH' in os.environ else ''
         os.environ['PYTHONPATH'] = FAIRSEQ_PATH + ':' + original_pythonpath
