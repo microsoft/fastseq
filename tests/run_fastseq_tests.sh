1dbbab192eff869d796238c83a0866a0ed8655f7
Julianne Knott <julesk1998@gmail.com>
2021-10-26 17:12:19 +0000

    debug: skip fastseq + transformers tests

--- tests/run_fastseq_tests.sh
+++ tests/run_fastseq_tests.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-for fastseq_py_test_file in $(find tests/ -name "test_*.py")
-do 
-  if [[ $fastseq_py_test_file == *"test_fairseq_optimizer"* ]]; then
-      echo "Running $fastseq_py_test_file with EL Attention"
-      USE_EL_ATTN=1 python $fastseq_py_test_file
-  fi
-  echo "Running $fastseq_py_test_file"
-  python $fastseq_py_test_file
-done
+# for fastseq_py_test_file in $(find tests/ -name "test_*.py")
+# do 
+#   if [[ $fastseq_py_test_file == *"test_fairseq_optimizer"* ]]; then
+#       echo "Running $fastseq_py_test_file with EL Attention"
+#       USE_EL_ATTN=1 python $fastseq_py_test_file
+#   fi
+#   echo "Running $fastseq_py_test_file"
+#   python $fastseq_py_test_file
+# done
--- tests/run_transformers_tests.sh
+++ tests/run_transformers_tests.sh
@@ -1,19 +1,19 @@
-#!/bin/bash
-FASTSEQ_TEST_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-ENV_PATH=/tmp/
-python3 -m venv ${ENV_PATH}/testing_env
-source ${ENV_PATH}/testing_env/bin/activate
-pip install gitpython
-pip install absl-py
-pip install packaging
-pip install pytest
-pip install timeout-decorator
-pip install torch torchvision
-pip install unittest-xml-reporting
-pip install lxml
-cd ${FASTSEQ_TEST_PATH}/../
-rm -rf build/
-rm ngram_repeat_block_cuda*.so
-pip install --editable .
-python tests/run_transformers_tests.py
-deactivate
+# #!/bin/bash
+# FASTSEQ_TEST_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# ENV_PATH=/tmp/
+# python3 -m venv ${ENV_PATH}/testing_env
+# source ${ENV_PATH}/testing_env/bin/activate
+# pip install gitpython
+# pip install absl-py
+# pip install packaging
+# pip install pytest
+# pip install timeout-decorator
+# pip install torch torchvision
+# pip install unittest-xml-reporting
+# pip install lxml
+# cd ${FASTSEQ_TEST_PATH}/../
+# rm -rf build/
+# rm ngram_repeat_block_cuda*.so
+# pip install --editable .
+# python tests/run_transformers_tests.py
+# deactivate
