645887ba7758e62486dd6dc12ed166a9e8a39e17
Julianne Knott <julesk1998@gmail.com>
2021-12-09 21:36:28 +0000

    pipeline debug

--- tests/run_fastseq_tests.sh
+++ tests/run_fastseq_tests.sh
@@ -4,8 +4,8 @@ for fastseq_py_test_file in $(find tests/ -name "test_*.py")
 do 
   if [[ $fastseq_py_test_file == *"test_fairseq_optimizer"* ]]; then
       echo "Running $fastseq_py_test_file with EL Attention"
-      USE_EL_ATTN=1 python3 $fastseq_py_test_file
+      PYTHONIOENCODING=utf8 USE_EL_ATTN=1 python3 $fastseq_py_test_file
   fi
   echo "Running $fastseq_py_test_file"
-  python3 $fastseq_py_test_file
+  PYTHONIOENCODING=utf8 python3 $fastseq_py_test_file
 done
